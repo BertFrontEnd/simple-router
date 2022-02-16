5f841a77eb3b1f41999774528c829a851156e0b7
BertFrontEnd <48886417+BertFrontEnd@users.noreply.github.com>
2022-02-16 10:03:20 +0300

    feat: add components

---
+++ script.js
@@ -0,0 +1,45 @@
+// Components
+
+const HomeComponents = {
+  render: () => {
+    return `
+      <section>
+        <h1>Home</h1>
+        <p>This is just a test</p>
+      </section>
+    `;
+  },
+};
+
+const Page1Components = {
+  render: () => {
+    return `
+      <section>
+        <h1>Page 1</h1>
+        <p>This is just a test of Page 1</p>
+      </section>
+    `;
+  },
+};
+
+const Page2Components = {
+  render: () => {
+    return `
+      <section>
+        <h1>Page 2</h1>
+        <p>This is just a test of Page 2</p>
+      </section>
+    `;
+  },
+};
+
+const ErrorComponents = {
+  render: () => {
+    return `
+      <section>
+        <h1>Error</h1>
+        <p>This is just a test of Error</p>
+      </section>
+    `;
+  },
+};
